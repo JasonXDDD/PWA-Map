7e877090f5bdc5fe1ce2846a5edad358dfbd1739
Len_AshBell <jason1022liu@gmail.com>
2019-11-25 12:53:59 +0800

    change orign to static && add fcm to project

--- PWA-Map/src/app/app.component.ts
+++ PWA-Map/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { SwPush } from '@angular/service-worker';
-import * as firebase from 'firebase/app';
-import 'firebase/messaging';
-import { environment } from 'src/environments/environment';
+import { FirebaseMessageService } from './core/services/firebase-message.service';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +7,9 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  constructor(private swPush: SwPush) {}
+  constructor(private fcm: FirebaseMessageService) {}
 
   ngOnInit() {
-    firebase.initializeApp(environment.firebase);
-    const messaging = firebase.messaging();
-    navigator.serviceWorker.ready.then(registration => {
-      if (
-        !!registration &&
-        registration.active &&
-        registration.active.state &&
-        registration.active.state === 'activated'
-      ) {
-        messaging.useServiceWorker(registration);
-        messaging
-          .requestPermission()
-          .then(() => messaging.getToken())
-          .then(token => console.log('Permission granted!', token));
-      } else {
-        console.warn(
-          'No active service worker found, not able to get firebase messaging'
-        );
-      }
-    });
-
-    this.swPush.messages.subscribe(msg => {
-      console.log(msg);
-    });
+    this.fcm.init()
   }
 }
